5e9e5fe13ebb3163bf5a9db296e1898a4c313d07
Igor <nikolaevigor95@mail.ru>
2015-08-27 14:06:20 +0300

    Initial Commit

---
+++ shaurmApp/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  shaurmApp
+//
+//  Created by Igor Nikolaev on 27/08/15.
+//  Copyright (c) 2015 Nikolaev Igor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ shaurmApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  shaurmApp
+//
+//  Created by Igor Nikolaev on 27/08/15.
+//  Copyright (c) 2015 Nikolaev Igor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
